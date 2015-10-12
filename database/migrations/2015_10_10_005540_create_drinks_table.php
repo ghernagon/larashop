aa27dfa235a6f3f3a9d074a8124d9b8f7649b8c0
Gerardo Hernández <ghernagon@gmail.com>
2015-10-12 17:48:27 -0430

    Creating migrator and seed files

--- database/migrations/2015_10_10_005540_create_drinks_table.php
+++ database/migrations/2015_10_10_005540_create_drinks_table.php
@@ -3,16 +3,21 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDrinksTable extends Migration
-{
+class CreateDrinksTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
+    public function up() {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',75)->unique();
+            $table->text('comments')->nullable();
+            $table->integer('rating');
+            $table->date('brew_date');
+            $table->timestamps();        });
     }
 
     /**
@@ -20,8 +25,7 @@ class CreateDrinksTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        //
+    public function down() {
+        Schema::drop('drinks');
     }
 }
